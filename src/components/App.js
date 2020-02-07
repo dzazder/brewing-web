0bd18b01e964119e51c0c34d60ce6eac2c73d0cd
Bartosz Lampart <dzazder@gmail.com>
2020-02-07 15:27:43 +0100

    template

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+        //this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+        return (
+          <div className="app-root">
+            <h1>Welcome</h1>
+          </div>
+        );
+      }
+}
+
+export default App;
